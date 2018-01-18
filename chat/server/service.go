5ff76b3c21c5168b93b92186c6a5c641a4edb821
liangpengcheng <5459489@qq.com>
2018-01-18 18:23:09 +0800

    目录

--- chat/server/service.go
+++ chat/server/service.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/golang/protobuf/proto"
 	"github.com/liangpengcheng/qcontinuum/base"
+	"github.com/liangpengcheng/qserver/chat"
 	"github.com/liangpengcheng/qserver/service"
 )
 
@@ -17,7 +18,7 @@ func newChatService() *service.Manager {
 	serv := &chatService{
 		handler: handler,
 	}
-	handler.AddCallback(int32(C2GIMMessage_ID), serv.onChat)
+	handler.AddCallback(int32(chat.C2GIMMessage_ID), serv.onChat)
 	manager := service.NewManager(serv, "127.0.0.1:8888", "127.0.0.1:7777")
 	serv.manager = manager
 	return manager
@@ -34,18 +35,18 @@ func (cs *chatService) GetProcessor() *service.RPCHandler {
 }
 
 func (cs *chatService) onChat(msg *service.RPCMessage) (proto.Message, int32) {
-	chat := C2GIMMessage{}
-	err := proto.Unmarshal(msg.MSG, &chat)
-	chatresult := G2CIMChatResult{}
+	chatmsg := chat.C2GIMMessage{}
+	err := proto.Unmarshal(msg.MSG, &chatmsg)
+	chatresult := chat.G2CIMChatResult{}
 	if base.CheckError(err, "unmarshal chat") {
-		chatto := G2CIMMessage{}
+		chatto := chat.G2CIMMessage{}
 		chatto.From = msg.UID
-		chatto.Content = chat.GetContent()
-		chatto.Channel = chat.GetChannel()
-		cs.manager.SendMessageTo(chat.GetTouser(), &chatto, int32(G2CIMMessage_ID))
+		chatto.Content = chatmsg.GetContent()
+		chatto.Channel = chatmsg.GetChannel()
+		cs.manager.SendMessageTo(chatmsg.GetTouser(), &chatto, int32(chat.G2CIMMessage_ID))
 		chatresult.Result = "Success"
 	} else {
 		chatresult.Result = "Failed"
 	}
-	return &chatresult, int32(G2CIMChatResult_ID)
+	return &chatresult, int32(chat.G2CIMChatResult_ID)
 }
